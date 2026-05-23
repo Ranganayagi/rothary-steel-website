32f2223872f0b4f81cfec1e370528a4cb0dba35c
Ranganayagi Jayaraman <jranganayagi@gmail.com>
2026-05-24 04:27:51 +0800

    website updated

--- app/page.js
+++ app/page.js
@@ -490,15 +490,33 @@ Message: ${formData.message}`
               </a>
             </div>
 
-            <div className="mt-10 pt-8 border-t border-white/10 text-center text-gray-300 space-y-3">
-              <p>
-                📍 Lot 4337, Batu 6, Jalan Maharajalela, Kampung Sri Jaya,
-                36000 Teluk Intan, Perak.
-              </p>
-              <p>📞 +60 11 3602 3592</p>
-              <p>✉️ rotharysteeltechengineering@gmail.com</p>
-              <p>Service Area: Available across Malaysia.</p>
-            </div>
+            {/* GOOGLE MAP */}
+<div className="mt-10 rounded-3xl overflow-hidden shadow-xl border border-white/10">
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.339582749128!2d100.96669177497499!3d3.950229596023524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cb415a2cfccc9d%3A0x2592c7950c1acf53!2sRothary%20Steel%20Tech%20Engineering!5e0!3m2!1sen!2smy!4v1779567714298!5m2!1sen!2smy"
+    width="100%"
+    height="350"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+    className="w-full"
+  ></iframe>
+</div>
+
+{/* COMPANY INFO */}
+<div className="mt-10 pt-8 border-t border-white/10 text-center text-gray-300 space-y-3">
+  <p>
+    📍 Lot 4337, Batu 6, Jalan Maharajalela, Kampung Sri Jaya,
+    36000 Teluk Intan, Perak.
+  </p>
+
+  <p>📞 +60 11 3602 3592</p>
+
+  <p>✉️ rotharysteeltechengineering@gmail.com</p>
+
+  <p>Service Area: Available across Malaysia.</p>
+</div>
           </div>
         </div>
       </section>
