447448e76dd0aed9fd8146e8f9838cd3b9243ab5
Ranganayagi Jayaraman <jranganayagi@gmail.com>
2026-05-25 00:36:47 +0800

    website updated

--- app/page.js
+++ app/page.js
@@ -367,6 +367,45 @@ Message: ${formData.message}`
         </div>
       </section>
 
+      {/* PHOTO GALLERY */}
+<section className="py-16 md:py-24 px-5 md:px-6 bg-gray-100">
+  <div className="max-w-7xl mx-auto">
+    <h2 className="text-4xl md:text-5xl font-black text-center text-[#06142b] mb-4">
+      PHOTO GALLERY
+    </h2>
+
+    <p className="text-center text-gray-600 text-lg mb-12">
+      Our latest steel fabrication, welding and engineering projects.
+    </p>
+
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      {[
+        "/image2.jpg",
+        "/image4.jpg",
+        "/image5.jpg",
+        "/image6.jpg",
+        "/image8.jpg",
+        "/image9.jpg",
+        "/image10.jpg",
+        "/image11.jpg",
+      ].map((img, index) => (
+        <div
+          key={index}
+          className="group overflow-hidden rounded-3xl shadow-xl bg-white"
+        >
+          <div className="h-56 md:h-64 overflow-hidden">
+            <img
+              src={img}
+              alt="Rothary Steel Project"
+              className="w-full h-full object-cover object-center transition duration-700 group-hover:scale-110"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
+
       {/* WHY CHOOSE US */}
       <section className="py-16 md:py-24 px-5 md:px-6 bg-[#082318] text-white">
         <div className="max-w-7xl mx-auto">
@@ -546,7 +585,7 @@ Message: ${formData.message}`
             <img
               src="/facebook.png"
               alt="Facebook"
-              className="w-8 h-8 hover:scale-110 transition object-contain"
+              className="w-9 h-9 hover:scale-110 transition object-contain"
             />
           </a>
 
@@ -558,7 +597,7 @@ Message: ${formData.message}`
             <img
               src="/instagram.png"
               alt="Instagram"
-              className="w-8 h-8 hover:scale-110 transition object-contain"
+              className="w-9 h-9 hover:scale-110 transition object-contain"
             />
           </a>
 
@@ -570,7 +609,7 @@ Message: ${formData.message}`
             <img
               src="/tiktok.png"
               alt="TikTok"
-              className="w-8 h-8 hover:scale-110 transition object-contain"
+              className="w-9 h-9 hover:scale-110 transition object-contain"
             />
           </a>
         </div>
